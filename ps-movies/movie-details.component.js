36a10497303dc2034bd0516eb5f9c8263f90e26d
Ahmad Albakri <ahmad.albakri@exact.com>
2016-05-27 18:42:35 +0800

    adjust detail page

--- ps-movies/movie-details.component.js
+++ ps-movies/movie-details.component.js
@@ -1,11 +1,54 @@
 (function(){
 	"use strict";
 	
-	var module = angular.module("psMovies");	
+	var module = angular.module("psMovies");
+
+	function detailController($http){
+		var model = this;
+		model.movies = [];
+		
+		model.$routerOnActivate = function(next){ // next : current, previous : coming from
+			model.id = next.params.id;
+		};
+
+		model.$onInit = function(){
+			fetchMovies($http).then(function(movies){
+				model.movies = movies;
+			});
+		};
+		console.log(model.movies);
+	};
+
+	/*
+	function controller($http){			
+		var model = this;
+		model.movies = [];
+		
+		fetchMovies($http).then(function(movies){
+			model.movies = movies;
+		});
+	};
+	*/
+	
+	function fetchMovies($http){
+		return $http.get("/angular/movies.json")
+		.then(function(response){
+			return response.data
+		});
+	};
 	
 	module.component("movieDetails", {
 		templateUrl : "/angular/ps-movies/movie-details.component.html",
-		
+		/*
+		// execute before $routerOnActivate, get http from server to check can proceed(with auth) or not
+		$canActivate : function($timeout){
+			return $timeout(function(){
+				return true;
+			}, 2000);
+		}
+		*/
+		controllerAs : "model",
+		controller : ["$http", detailController]
 	});
 	
 }());
